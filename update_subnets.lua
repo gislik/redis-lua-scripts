9cee28d2294b3339068a37978d44bf41c55e7bb7
Gísli Kristjánsson <gislik@hamstur.is>
2014-01-19 15:19:35 +0000

    Reporting added to update_subnets.lua

--- update_subnets.lua
+++ update_subnets.lua
@@ -10,10 +10,12 @@ for i, addresses in ipairs(redis.call("keys", "mo:user:*:addresses")) do
   local prefix, id = string.match(addresses, "(.+:(%d+)):addresses$")
   if prefix ~= nil then    
     local userkey = prefix..":subnets"
+    table.insert(t, userkey)
     redis.call("del", userkey)
     for j, ip in ipairs(redis.call("hkeys", addresses)) do
       local s = subnet(ip, m)..":"..m
       local subnetkey = "mo:subnets:"..s
+      table.insert(t, subnetkey)
       redis.call("sadd", userkey, s)
       redis.call("sadd", subnetkey, id)
     end
